f52081a510c1d3c93b05f0acef081999da61e5d9
oh-yuya <happy.cccccc.lemon@gmail.com>
2021-07-05 19:01:51 +0900

    init

---
+++ alp_setup.sh
@@ -0,0 +1,30 @@
+# Install apl
+echo 'Install alp'
+wget https://github.com/tkuchiki/alp/releases/download/v1.0.4/alp_linux_amd64.zip
+unzip alp_linux_amd64.zip
+
+# パスの通っているディレクトリに移動
+sudo mv alp /usr/local/bin/alp
+
+alp --version && echo "Success Install alp"
+
+sudo touch /etc/nginx/conf.d/log_format.conf
+sudo chmod 777 /etc/nginx/conf.d/log_format.conf
+sudo echo 'log_format ltsv "time:$time_local"
+                "\thost:$remote_addr"
+                "\tforwardedfor:$http_x_forwarded_for"
+                "\treq:$request"
+                "\tstatus:$status"
+                "\tmethod:$request_method"
+                "\turi:$request_uri"
+                "\tsize:$body_bytes_sent"
+                "\treferer:$http_referer"
+                "\tua:$http_user_agent"
+                "\treqtime:$request_time"
+                "\tcache:$upstream_http_x_cache"
+                "\truntime:$upstream_http_x_runtime"
+                "\tapptime:$upstream_response_time"
+                "\tvhost:$host";
+access_log /var/log/nginx/access.log ltsv;' > /etc/nginx/conf.d/log_format.conf
+
+echo "You have to edit /etc/nginx/nginx.conf"
